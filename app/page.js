ed854869acb4d54e16f77d813542a5b29b26d3be
Daniel Santamaria <santamaria.daniel316@gmail.com>
2024-09-01 15:07:28 -0700

    add submit button

--- app/page.js
+++ app/page.js
@@ -1,10 +1,19 @@
 'use client'
+import { Button } from '@nextui-org/react'
 import DropZone from './components/DropZone'
 import ThemeSwitcher from './components/ThemeSwitcher'
 
 
 export default function Home() {
-  const handleFileChange = () => {}
+  const handleFileChange = () => {
+
+
+  }
+
+  const handleSubmit = () => {
+
+ 
+  }
   return (
     <div className='flex w-screen h-screen flex-col justify-center items-center gap-10'>
         <div className='flex items-center gap-2'>
@@ -14,6 +23,7 @@ export default function Home() {
         <p className='text-xl'>Submit a document and let us do the rest.</p>
 
         <DropZone> <input type="file"  name="image"  onChange={handleFileChange} className={`block w-full text-M text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-primary file:text-white`} /> </DropZone>
+        <Button color='primary' onClick={handleSubmit}>Submit</Button>
     </div>
 
   )
